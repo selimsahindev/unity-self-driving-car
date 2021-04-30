ffdc1b28f0fcd1ca254af14d9a30364c5444f9b4
selimsahindev <selimsahin.sns@gmail.com>
2021-05-01 00:17:04 +0300

    car model, temporary roads and basic car controller added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Camera cam1;
+    public Camera cam2;
+
+    private void Start() {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            SwitchCamera();
+        }
+    }
+
+    private void SwitchCamera() {
+        if (cam1.enabled) {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        } else {
+            cam1.enabled = true;
+            cam2.enabled = false;
+        }
+    }
+}
