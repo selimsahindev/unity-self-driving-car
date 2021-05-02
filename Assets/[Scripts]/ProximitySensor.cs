3579e494365785da36e820e1b5bb065e9383d36c
selimsahindev <selimsahin.sns@gmail.com>
2021-05-02 23:46:02 +0300

    minor changes on gizmos

--- Assets/[Scripts]/ProximitySensor.cs
+++ Assets/[Scripts]/ProximitySensor.cs
@@ -7,6 +7,9 @@ public class ProximitySensor : MonoBehaviour {
 
     [HideInInspector] public float sensorA, sensorB, sensorC;
 
+    // these points are only for use in the OnDrawGizmos method
+    private Vector3 hitPointA, hitPointB, hitPointC;
+
     private void FixedUpdate() {
         InputSensors();
     }
@@ -21,8 +24,10 @@ public class ProximitySensor : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, sensorRange)) {
             sensorA = hit.distance / 20f;
-            Debug.DrawLine(transform.position, transform.position + a * sensorRange, Color.green);
+            hitPointA = hit.point;
+            Debug.DrawLine(transform.position, hit.point, Color.green);
         } else {
+            hitPointA = Vector3.zero;
             Debug.DrawLine(transform.position, transform.position + a * sensorRange, Color.red);
         }
 
@@ -30,8 +35,10 @@ public class ProximitySensor : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, sensorRange)) {
             sensorB = hit.distance / 20f;
-            Debug.DrawLine(transform.position, transform.position + b * sensorRange, Color.green);
+            hitPointB = hit.point;
+            Debug.DrawLine(transform.position, hit.point, Color.green);
         } else {
+            hitPointB = Vector3.zero;
             Debug.DrawLine(transform.position, transform.position + b * sensorRange, Color.red);
         }
 
@@ -39,11 +46,26 @@ public class ProximitySensor : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, sensorRange)) {
             sensorC = hit.distance / 20f;
-            Debug.DrawLine(transform.position, transform.position + c * sensorRange, Color.green);
+            hitPointC = hit.point;
+            Debug.DrawLine(transform.position, hit.point, Color.green);
         } else {
+            hitPointC = Vector3.zero;
             Debug.DrawLine(transform.position, transform.position + c * sensorRange, Color.red);
         }
 
         print("A: " + sensorA + "\tB: " + sensorB + "\tC: " + sensorC);
+    }
+
+    private void OnDrawGizmos() {
+        float sphereRadius = 0.3f;
+        Gizmos.color = Color.green;
+
+        if (hitPointA.magnitude > 0f)
+            Gizmos.DrawWireSphere(hitPointA, sphereRadius);
+        if (hitPointB.magnitude > 0f)
+            Gizmos.DrawWireSphere(hitPointB, sphereRadius);
+        if (hitPointC.magnitude > 0f)
+            Gizmos.DrawWireSphere(hitPointC, sphereRadius);
+
     }
 }
