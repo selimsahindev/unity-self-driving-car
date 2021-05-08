7ef35086b78d9c455cc81295c3f3477407d47af4
Selim Şahin <selimsahin.sns@gmail.com>
2021-05-08 20:18:58 +0300

    implemented crossover and mutation

--- Assets/[Scripts]/GeneticManager.cs
+++ Assets/[Scripts]/GeneticManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using MathNet.Numerics.LinearAlgebra;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneticManager : MonoBehaviour {
@@ -78,6 +79,99 @@ public class GeneticManager : MonoBehaviour {
         SortPopulation();
 
         NNet[] newPopulation = PickBestPopulation();
+
+        Crossover(newPopulation);
+        Mutate(newPopulation);
+
+        FillPopulationWithRandomValues(newPopulation, naturallySelected);
+
+        population = newPopulation;
+
+        currentGenome = 0;
+
+        ResetToCurrentGenome();
+    }
+
+    private void Crossover(NNet[] newPopulation) {
+        for (int i = 0; i < numberToCrossover; i += 2) {
+            // These are indexes of the parents.
+            int indexA = i;
+            int indexB = i + 1;
+
+            if (genePool.Count >= 1) {
+                // We need to ensure that these are not the same.
+                // Because if two parents are the same, we're not gonna get a new child.
+                indexA = genePool[Random.Range(0, genePool.Count)];
+                do
+                    indexB = genePool[Random.Range(0, genePool.Count)];
+                while (indexA == indexB);
+            }
+
+            NNet child1 = new NNet();
+            NNet child2 = new NNet();
+
+            child1.Initialize(carBrain.layers, carBrain.neurons);
+            child2.Initialize(carBrain.layers, carBrain.neurons);
+
+            child1.fitness = 0;
+            child2.fitness = 0;
+
+            for (int w = 0; w < child1.weights.Count; w++) {
+                if (Random.Range(0f, 1f) < 0.5f) {
+                    child1.weights[w] = population[indexA].weights[w];
+                    child2.weights[w] = population[indexB].weights[w];
+                } else {
+                    child1.weights[w] = population[indexB].weights[w];
+                    child2.weights[w] = population[indexA].weights[w];
+                }
+            }
+
+            for (int b = 0; b < child1.biases.Count; b++) {
+                if (Random.Range(0f, 1f) < 0.5f) {
+                    child1.biases[b] = population[indexA].biases[b];
+                    child2.biases[b] = population[indexB].biases[b];
+                } else {
+                    child1.biases[b] = population[indexB].biases[b];
+                    child2.biases[b] = population[indexA].biases[b];
+                }
+            }
+
+            newPopulation[naturallySelected] = child1;
+            naturallySelected++;
+
+            newPopulation[naturallySelected] = child2;
+            naturallySelected++;
+        }
+
+    }
+
+    private void Mutate(NNet[] newPopulation) {
+        for (int i = 0; i < naturallySelected; i++) {
+
+            for (int c = 0; c < newPopulation[i].weights.Count; c++) {
+
+                if (Random.Range(0f, 1f) < mutationRate) {
+                    newPopulation[i].weights[c] = MutateMatrix(newPopulation[i].weights[c]);
+                }
+
+            }
+
+        }
+    }
+
+    private Matrix<float> MutateMatrix(Matrix<float> mat) {
+        int randomPoints = (int) Random.Range(1f, mat.RowCount * mat.ColumnCount / 7);
+
+        Matrix<float> temp = mat;
+        
+        for (int i = 0; i < randomPoints; i++) {
+            int randomColumn = (int) Random.Range(0f, temp.ColumnCount);
+            int randomRow = (int) Random.Range(0f, temp.RowCount);
+
+            temp[randomRow, randomColumn] = Mathf.Clamp(temp[randomRow, randomColumn] + Random.Range(-1f, 1f), -1f, 1f);
+        }
+
+        return temp;
     }
 
     private NNet[] PickBestPopulation() {
